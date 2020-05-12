19a6b68c2e864df673ac96d200223e733d98dc3a
ljy <1359981767@qq.com>
2020-05-12 13:25:52 +0800

    主函数

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include"LexicalAnalyser.h"
+#include "ParserAndSemanticAnalyser.h"
+#include "ObjectCodeGenarator.h"
+int main() {
+	LexicalAnalyser lexicalAnalyser("test.txt");
+	lexicalAnalyser.analyse();
+	
+	ParserAndSemanticAnalyser parserAndSemanticAnalyser("productions.txt");
+	//parserAndSemanticAnalyser.outputDFA("DFA.txt");
+	parserAndSemanticAnalyser.analyse(lexicalAnalyser.getResult(), "SLR1_analyse.txt");
+	//parserAndSemanticAnalyser.outputIntermediateCode();
+	
+	IntermediateCode* code = parserAndSemanticAnalyser.getIntermediateCode();
+	code->divideBlocks(parserAndSemanticAnalyser.getFuncEnter());
+	//code->outputBlocks();
+
+	ObjectCodeGenerator objectCodeGenerator;
+	objectCodeGenerator.analyseBlock(code->getFuncBlock());
+	objectCodeGenerator.outputIBlocks();
+	objectCodeGenerator.generateCode();
+	objectCodeGenerator.outputObjectCode();
+	objectCodeGenerator.outputObjectCode("program.asm");
+
+	return 0;
+}
